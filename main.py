82fbb1760f45306b501c0f3cd4c1ce997fab2a72
TWITCHGAMER34 <nazoma05@gmail.com>
2023-03-22 14:40:22 +0100

    Började göra startskärm samt började med andra skärmen

--- main.py
+++ main.py
@@ -1,16 +1,80 @@
-# This is a sample Python script.
+import tkinter as tk
+import tkinter.font as tkFont
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Game:
+    def __init__(self, root):
+        #setting title
+        root.title("The girl who is missing")
+        #setting window size
+        width=600
+        height=243
+        screenwidth = root.winfo_screenwidth()
+        screenheight = root.winfo_screenheight()
+        alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+        root.configure(bg = "#000000")
+        root.geometry(alignstr)
+        root.resizable(width=False, height=False)
+
+        GMessage_405=tk.Message(root)
+        GMessage_405["anchor"] = "center"
+        GMessage_405["bg"] = "#000000"
+        ft = tkFont.Font(family='Times',size=28)
+        GMessage_405["font"] = ft
+        GMessage_405["fg"] = "#ffffff"
+        GMessage_405["justify"] = "center"
+        GMessage_405["text"] = "The girl who is missing"
+        GMessage_405.place(x=110,y=20,width=357,height=90)
+
+        start=tk.Button(root)
+        start["activebackground"] = "#474747"
+        start["bg"] = "#000000"
+        ft = tkFont.Font(family='Times',size=10)
+        start["font"] = ft
+        start["fg"] = "#ffffff"
+        start["justify"] = "center"
+        start["text"] = "Klick here to play!"
+        start.place(x=240,y=140,width=118,height=52)
+        start["command"] = self.start
+
+        GButton_701=tk.Button(root)
+        GButton_701["activebackground"] = "#000000"
+        GButton_701["activeforeground"] = "#ffffff"
+        GButton_701["bg"] = "#ff0000"
+        ft = tkFont.Font(family='Times',size=10)
+        GButton_701["font"] = ft
+        GButton_701["fg"] = "#ffffff"
+        GButton_701["justify"] = "center"
+        GButton_701["text"] = "Quit!"
+        GButton_701.place(x=0,y=200,width=94,height=36)
+        GButton_701["command"] = self.GButton_701_command
+
+    def window(self):
+        root = tk.Tk()
+        root.title("The girl who got missing")
+        #setting window size
+        width=600
+        height=243
+        screenwidth = root.winfo_screenwidth()
+        screenheight = root.winfo_screenheight()
+        alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+        root.configure(bg = "#000000")
+        root.geometry(alignstr)
+        root.resizable(width=False, height=False)
+
+    def Game_0(self):
+        self.window()
+        root.destroy()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+    def start(self):
+        self.Game_0()
+
+    def GButton_701_command(self):
+        quit()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = Game(root)
+    root.mainloop()
