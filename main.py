84deb3f4b0de6516e70fb9ea69977acfee29a4b8
TWITCHGAMER34 <nazoma05@gmail.com>
2023-03-24 11:42:54 +0100

    Gjorde flera game states samt har gjort class för Karaktär, och subklasser till den är PLayer, Enemy, item

--- main.py
+++ main.py
@@ -4,22 +4,24 @@ This is the main file of the game
 
 import tkinter as tk
 import tkinter.font as tkFont
+from player import Character, Player, Enemy, Item
+
 
 class Game:
     def __init__(self, root):
-        #setting title
+        # setting title
         root.title("The girl who is missing")
-        #setting window size
-        width=1200
-        height=600
+        # setting window size
+        width = 1200
+        height = 600
         screenwidth = root.winfo_screenwidth()
         screenheight = root.winfo_screenheight()
         alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
-        root.configure(bg = "#000000")
+        root.configure(bg="#000000")
         root.geometry(alignstr)
         root.resizable(width=False, height=False)
 
-        GMessage_405=tk.Message(root)
+        GMessage_405 = tk.Message(root)
         GMessage_405["anchor"] = "center"
         GMessage_405["bg"] = "#000000"
         ft = tkFont.Font(family='Times', size=50, weight="bold", slant="italic", underline=True)
@@ -28,30 +30,30 @@ class Game:
         GMessage_405["justify"] = "center"
         GMessage_405["width"] = 1000
         GMessage_405["text"] = "The girl who is missing"
-        GMessage_405.place(x=110,y=100,width=1000,height=90)
+        GMessage_405.place(x=110, y=100, width=1000, height=90)
 
-        start=tk.Button(root)
+        start = tk.Button(root)
         start["activebackground"] = "#474747"
         start["bg"] = "#000000"
-        ft = tkFont.Font(family='Times',size=10)
+        ft = tkFont.Font(family='Times', size=10)
         start["font"] = ft
         start["fg"] = "#ffffff"
         start["justify"] = "center"
         start["text"] = "Klick here to play!"
-        start.place(x=500,y=200,width=200,height=100)
+        start.place(x=500, y=200, width=200, height=100)
         start["command"] = self.start
 
-        GButton_701=tk.Button(root)
-        GButton_701["activebackground"] = "#000000"
-        GButton_701["activeforeground"] = "#ffffff"
-        GButton_701["bg"] = "#ff0000"
-        ft = tkFont.Font(family='Times',size=10)
-        GButton_701["font"] = ft
-        GButton_701["fg"] = "#ffffff"
-        GButton_701["justify"] = "center"
-        GButton_701["text"] = "Quit!"
-        GButton_701.place(x=0,y=550,width=150,height=50)
-        GButton_701["command"] = self.GButton_701_command
+        quit = tk.Button(root)
+        quit["activebackground"] = "#000000"
+        quit["activeforeground"] = "#ffffff"
+        quit["bg"] = "#ff0000"
+        ft = tkFont.Font(family='Times', size=10)
+        quit["font"] = ft
+        quit["fg"] = "#ffffff"
+        quit["justify"] = "center"
+        quit["text"] = "Quit!"
+        quit.place(x=0, y=550, width=150, height=50)
+        quit["command"] = self.quit
 
     def window(self):
         """
@@ -60,17 +62,17 @@ class Game:
         """
         root = tk.Tk()
         root.title("The girl who got missing")
-        #setting window size
-        width=600
-        height=243
+        # setting window size
+        width = 600
+        height = 243
         screenwidth = root.winfo_screenwidth()
         screenheight = root.winfo_screenheight()
         alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
-        root.configure(bg = "#000000")
+        root.configure(bg="#000000")
         root.geometry(alignstr)
         root.resizable(width=False, height=False)
 
-    def setupText(self, input):
+    def setupText(self, input, fontsize):
         """
 
         :param input:
@@ -79,7 +81,7 @@ class Game:
         text = tk.Message(root)
         text["anchor"] = "center"
         text["bg"] = "#000000"
-        ft = tkFont.Font(family='Times', size=28)
+        ft = tkFont.Font(family='Times', size=fontsize)
         text["font"] = ft
         text["fg"] = "#ffffff"
         text["justify"] = "center"
@@ -87,7 +89,7 @@ class Game:
         text["text"] = input
         return text
 
-    def setupButton(self, input, command):
+    def setupButton(self, input, command, fontsize):
         """
 
         :param input:
@@ -96,7 +98,7 @@ class Game:
         button = tk.Button(root)
         button["activebackground"] = "#474747"
         button["bg"] = "#000000"
-        ft = tkFont.Font(family='Times', size=10)
+        ft = tkFont.Font(family='Times', size=fontsize)
         button["font"] = ft
         button["fg"] = "#ffffff"
         button["justify"] = "center"
@@ -120,7 +122,7 @@ class Game:
 
         return entry
 
-    def destroy(self): #destroy all widgets
+    def destroy(self):  # destroy all widgets
         """
 
         :return:
@@ -128,37 +130,90 @@ class Game:
         for child in root.winfo_children():
             child.destroy()
 
-
-
     def main(self):
-        self.destroy() #destroy all widgets
-        GMessage_405 = self.setupText('How old are you?')
-        GMessage_405.place(x=110,y=20,width=357,height=120)
+        self.destroy()  # destroy all widgets
+        GMessage_405 = self.setupText('How old are you?', fontsize=20)
+        GMessage_405.place(x=140, y=50, width=357, height=120)
+
+        Gmessage_406 = self.setupText('Whats your name?', fontsize=20)
+        Gmessage_406.place(x=650, y=50, width=357, height=120)
+
+        self.GEntry_1 = self.setupEntry()
+        self.GEntry_1.place(x=780, y=140, width=118, height=52)
 
         self.GEntry_0 = self.setupEntry()
-        self.GEntry_0.place(x=240,y=140,width=118,height=52)
+        self.GEntry_0.place(x=240, y=140, width=118, height=52)
 
-        GButton_0 = self.setupButton('next', self.state2)
-        GButton_0.place(x=240,y=200,width=118,height=52)
-
-
-
+        GButton_0 = self.setupButton('next', self.state2, fontsize=20)
+        GButton_0.place(x=500, y=350, width=118, height=52, )
 
     def state2(self):
         input = self.GEntry_0.get()
+        input2 = self.GEntry_1.get()
         self.destroy()
 
-        GMessage_405 = self.setupText(f"Hello bob, you are %s years old" % (input))
-        GMessage_405.place(x=110,y=20,width=357,height=120)
+        GMessage_405 = self.setupText(f'Hello %s, you are %s years old, correct?' % (input2, input), fontsize=30)
+        GMessage_405.place(x=400, y=20, width=357, height=300)
+
+        GButton_0 = self.setupButton('Yes', self.state3, fontsize=20)
+        GButton_0.place(x=450, y=350, width=118, height=52)
+
+        GButton_1 = self.setupButton('No', self.main, fontsize=20)
+        GButton_1.place(x=600, y=350, width=118, height=52)
 
 
+    def state3(self):
+        self.destroy()
+        GMessage_405 = self.setupText(
+            'You wake up in a dark room, you dont know where you are or how you got here, you see a door in front of you, to your right and to your left, choose a door.',
+            fontsize=33)
+        GMessage_405.place(x=-250, y=10, width=1100, height=600)
+
+        GButton_0 = self.setupButton('Open the door infront of you', self.state4, fontsize=15)
+        GButton_0.place(x=500, y=200, width=300, height=50)
+
+        GButton_1 = self.setupButton('Open the door to your right', self.state5, fontsize=15)
+        GButton_1.place(x=500, y=300, width=300, height=50)
+
+        GButton_2 = self.setupButton('Open the door to your left', self.state6, fontsize=15)
+        GButton_2.place(x=500, y=100, width=300, height=50)
+
+    def state4(self):
+        self.destroy()
+        GMessage_405 = self.setupText('You open the door in-front of you, you see a staircase, you go up the stairs and you see a door, you open the door and you see a room with a table and a chair, you sit down and you see a piece of paper on the table, you pick it up and it says "You are in a room, you have to find a way out, you can go up the stairs or you can go down the stairs, choose wisely"', fontsize=20)
+        GMessage_405.place(x=-250, y=20, width=1000, height=600)
+
+        GButton_0 = self.setupButton('Go up the stairs', self.state7, fontsize=20)
+        GButton_0.place(x=500, y=150, width=300, height=52)
+
+        GButton_1 = self.setupButton('Go down the stairs', self.state8, fontsize=20)
+        GButton_1.place(x=500, y=250, width=300, height=52)
+
+    def state5(self):
+        self.destroy()
+        GMessage_405 = self.setupText('', fontsize=20)
+        GMessage_405.place(x=-250, y=20, width=1000, height=600)
+
+        GButton_0 = self.setupButton('', self.state9, fontsize=20)
+        GButton_0.place(x=500, y=250, width=300, height=52)
+
+    def state6(self):
+        pass
+
+    def state7(self):
+        pass
+
+    def state8(self):
+        pass
+
+    def state9(self):
+        pass
 
     def start(self):
         self.main()
 
-    def GButton_701_command(self):
+    def quit(self):
         quit()
-
 
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,18 +1,107 @@
+"""
+
+"""
 import tkinter as tk
 import tkinter.font as tkFont
 
-class Player:
-    def __init__(self, health, inventory, armor):
+class Character:
+    def __init__(self, health, attack, defense, speed):
         self.health = health
+        self.attack = attack
+        self.defense = defense
+        self.speed = speed
+
+
+
+class Player(Character):
+    def __init__(self, health, attack, defense, speed, inventory):
+        super().__init__(health, attack, defense, speed)
         self.inventory = inventory
-        self.armor = armor
 
+    def add_item(self, item):
+        self.inventory.append(item)
 
+    def remove_item(self, item):
+        self.inventory.remove(item)
 
+    def get_inventory(self):
+        return self.inventory
 
+    def get_health(self):
+        return self.health
 
-class inventory(Player):
-    def __init__(self, item, health, inventory, armor):
-        super().__init__(health=100, inventory={}, armor=0)
-        self.item = item
+    def get_attack(self):
+        return self.attack
+
+    def get_defense(self):
+        return self.defense
+
+    def get_speed(self):
+        return self.speed
+
+    def set_health(self, health):
+        self.health = health
+
+    def set_attack(self, attack):
+        self.attack = attack
+
+    def set_defense(self, defense):
+        self.defense = defense
+
+    def set_speed(self, speed):
+        self.speed = speed
+
+    def __str__(self):
+        return f"Health: {self.health}, Attack: {self.attack}, Defense: {self.defense}, Speed: {self.speed}"
+
+class Enemy(Character):
+    def __init__(self, health, attack, defense, speed, name):
+        super().__init__(health, attack, defense, speed)
+        self.name = name
+
+    def get_health(self):
+        return self.health
+
+    def get_attack(self):
+        return self.attack
+
+    def get_defense(self):
+        return self.defense
+
+    def get_speed(self):
+        return self.speed
+
+    def set_health(self, health):
+        self.health = health
+
+    def set_attack(self, attack):
+        self.attack = attack
+
+    def set_defense(self, defense):
+        self.defense = defense
+
+    def set_speed(self, speed):
+        self.speed = speed
+
+    def __str__(self):
+        return f"Health: {self.health}, Attack: {self.attack}, Defense: {self.defense}, Speed: {self.speed}"
+
+class Item(object):
+    def __init__(self, name, description, value):
+        self.name = name
+        self.description = description
+        self.value = value
+
+    def get_name(self):
+        return self.name
+
+    def get_description(self):
+        return self.description
+
+    def get_value(self):
+        return self.value
+
+    def __str__(self):
+        return f"{self.name}: {self.description}. Value: {self.value}"
+
 
