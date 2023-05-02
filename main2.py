12d2aac7b488975bc245799c893bb4b3389b33bc
TWITCHGAMER34 <nazoma05@gmail.com>
2023-05-02 15:17:29 +0200

    Fixade pylint till 10/10 igen

--- main2.py
+++ main2.py
@@ -35,7 +35,8 @@ def weather():  # get weather from the api
     """
     access_key = "49fd86502d789706569dd23f26182bed"  # access key to the api
     query = "Karlstad"  # query to the api
-    link = f"http://api.weatherstack.com/current?access_key={access_key}&query={query}"  # link to the api
+    link = f"http://api.weatherstack.com/current?access_key={access_key}&query={query}"
+    # link to the api
     data = requests.get(link, timeout=5)  # get the data from the api
     t_t = json.loads(data.text)  # load the data
     return t_t  # return the data
